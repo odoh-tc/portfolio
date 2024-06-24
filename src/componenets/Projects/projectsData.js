315d290d9035fd0ca89d469495bd7364a23c105e
Tochukwu Odoh <cashearncrypto@gmail.com>
2024-06-24 18:23:00 +0100

    added weather api project

--- src/componenets/Projects/projectsData.js
+++ src/componenets/Projects/projectsData.js
@@ -3,25 +3,36 @@ import EcommerceImage from "../../assets/project_images/comm.jpeg";
 import InveImage from "../../assets/project_images/inventer.png";
 import FileImage from "../../assets/project_images/file.jpeg";
 import ChatImage from "../../assets/project_images/chat.jpeg";
+import WeatherImage from "../../assets/project_images/weather.jpeg";
 export const projectsData = [
   {
-    title: "E-commerce App",
+    title: "E-commerce API",
     description:
       "A robust e-commerce backend application. This app features role-based access control, allowing business owners to create and manage multiple businesses, each with its own products, while customers can place and manage orders. Additionally, an admin role is included to perform various administrative operations.",
     imgUrl: EcommerceImage,
     githubUrl: "https://github.com/odoh-tc/e-commerce",
-    liveUrl: "https://e-commerce-jzjf.onrender.com/docs",
+    liveUrl: "https://github.com/odoh-tc/e-commerce",
     techStack: ["FastAPI", "PostgreSQL", "SQLAlchemy", "JWT"],
     livePreview: true,
   },
   {
-    title: "File Uploader App",
+    title: "File Manager API",
     description:
       "This application provides a backend solution for uploading, managing, and sharing files, as well as managing users. It offers endpoints for user registration, authentication, user profile management, file upload, listing user files, listing all files (admin only), file analytics, file sharing, updating files, and deleting files.",
     imgUrl: FileImage,
     githubUrl: "https://github.com/odoh-tc/file_uploader",
     liveUrl: "https://github.com/odoh-tc/file_uploader",
     techStack: ["FastAPI", "MySQL", "SQLAlchemy", "JWT"],
+    livePreview: true,
+  },
+  {
+    title: "Weather API",
+    description:
+      "This application provides comprehensive weather data services, including current weather conditions, forecasts, air pollution metrics, historical weather data, UV index information, and geographical mapping. it employs caching mechanisms to enhance performance, and implements rate limiting to manage API request rates effectively.",
+    imgUrl: WeatherImage,
+    githubUrl: "https://github.com/odoh-tc/weather_api",
+    liveUrl: "https://github.com/odoh-tc/weather_api",
+    techStack: ["FastAPI", "Redis", "SQLAlchemy", "SQLite"],
     livePreview: true,
   },
   {
